9843da3d61ae3242bc0581e8b229650e224f3fa7
zhangshiyue <shiyue_zhang1011@163.com>
2019-04-11 15:32:23 +0800

    将工程添加到本地仓库

--- test/src/com/gsww/demo.java
+++ test/src/com/gsww/demo.java
@@ -3,5 +3,6 @@ package com.gsww;
 public class demo {
     public static void main(String[] args) {
         System.out.println("hello Git");
+        System.out.println("小米笔记本");
     }
 }
---
+++ test/src/com/gsww/demo01.java
@@ -0,0 +1,10 @@
+package com.gsww;
+
+public class demo01 {
+    public static void main(String[] args) {
+        System.out.println("aaaaaa");
+        System.out.println("aaaaaa");
+        System.out.println("aaaaaa");
+        System.out.println("aaaaaa");
+    }
+}
